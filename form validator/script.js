1bf76dd2a12676a0bb8989e081a8ecb4b0927545
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2023-11-09 09:09:53 +0300

    Form validation app finished

--- form validator/script.js
+++ form validator/script.js
@@ -6,54 +6,71 @@ const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
 // girdi ve bir mesaj alacak. değer girilmediğinde kırmızı rengi olacaktır(is-invalid)
-function error(input,message){
+function error(input, message) {
     input.className = 'form-control is-invalid';
     const div = input.nextElementSibling;
     div.innerText = message;
     div.className = 'invalid-feedback';
 }
-
 // girdi başarılı ise tik işareti ile doğru olduğunu bize anlatacaktır
-function success(input){
+function success(input) {
     input.className = 'form-control is-valid';
 }
 
 // bu fonksiyon email formatı için yazılmıştır ve hazır alınmıştır.kontrol eder ve küçük harfe çevirir
-function validateEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value)) {
+        success(input);
+    } else {
+        error(input, 'incorrect email address');
+    }
+}
+
+function checkRequired(inputs){
+    inputs.forEach(function(input){
+        if(input.value === '') {
+            error(input, `${input.id} is required.`);
+        }else{
+            success(input);
+        }
+    });  
+}
+
+function checkWordLenght(input,min,max){
+    if(input.value.lenght < min){
+        error(input,`${input.id} Must have a minimum of  ${min} characters`);
+    }else if(input.value.lenght > max){
+        error(input,`${input.id} Must have a maximum of  ${max} characters`);
+    }else{
+        success(input);
+    }
+}
+
+// girilen iki parola aynı mı kontrol edi-elim
+function checkPasswords(input1,input2){
+    if(input1.value !== input2.value){
+        error(input2,'Passwords are not the same!');
+    }
+}
+
+// phone için bazı validationlar vardır, onlar için yazılmış bir koddur
+// 10 karakterli olmalıdır vs.
+function checkPhoneNumber(input){
+    var phone = /^\d{10}$/;
+    if(!phone.test(input.value)){
+        error(input,` Enter your 10 character phone number`);
+    }
 }
 
 // Bir event oluşturuyorum. Submit butonuna tıklayınca bir olay gerçekleşecektir, bunun kodlarını yazalım.
-form.addEventListener('submit', function(e){
-    // formun varsayılan özelliğini kapatıp, submit etmemiş oluyoruz.
-    e.preventDefault(); 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    // Eğer username değeri boş ise error fonk çağır, değilse succes fonk çağır.
-    if(username.value === ''){
-        error(username,'Enter a valid username!');
-    }else{
-        success(username);
-    }
-
-    if(email.value === ''){
-        error(email,'Enter a valid email!');
-    }else if(!validateEmail(email.value)){
-        error(email,'Enter email in the correct format!');
-    }else{
-        success(email);
-    }
-    
-    if(password.value === ''){
-        error(password,'Enter a valid password!');
-    }else{
-        success(password);
-    }
-    
-    if(repassword.value === ''){
-        error(repassword,'Enter a valid password!');
-    }else{
-        success(repassword);
-    }
-
+    checkRequired([username,email,password,repassword,phone]);
+    checkEmail(email);
+    checkLength(username,7,15);
+    checkLength(password,5,12);
+    checkPasswords(password,repassword);
+    checkPhoneNumber(phone);
 });
